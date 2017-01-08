12a5254cfdd66a63502d5117956d6b923ea2c4e1
Donald <donald-one@hotmail.com>
2017-01-08 22:30:27 +0100

    Initial commit

---
+++ app/src/main/java/com/enterprise/donorapplication/LoginedActivity.java
@@ -0,0 +1,125 @@
+package com.enterprise.donorapplication;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+
+/**
+ * Created by donald on 17-01-08.
+ */
+
+public class LoginedActivity extends FragmentActivity {
+
+    private final static String SERVER_URL="serverIP";
+    static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
+    private final static String ACCESSINTOKEN="accesstoken";
+
+    private Button scanButton;
+
+    private boolean islogined;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logined);
+        scanButton = (Button) findViewById(R.id.scanqr);
+        islogined = true;
+
+        final IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                scanIntegrator.initiateScan();
+            }
+        });
+    }
+
+    private void showText(String message) {
+        Toast toast = Toast.makeText(getApplicationContext(),
+                message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.v("####","Request:"+requestCode+ "resultCOde:"+resultCode+" Data:"+data);
+        if(data != null && data.getAction() != null && data.getAction().equals(ACTION_SCAN)) //QR Code
+        {
+
+            IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+            if (scanningResult != null) {
+                String qrStr = scanningResult.getContents();
+                if(islogined == false)
+                {
+                    showText("User not Logged In");
+
+                }
+                else
+                {
+                    sendToServer(ACCESSINTOKEN, qrStr);
+                }
+                super.onActivityResult(requestCode, resultCode, data);
+            }
+            else
+            {
+                showText("No QR scan data received");
+
+
+            }
+
+        }
+        else if(data != null)
+        {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+
+    }
+
+
+    private void sendToServer(String accessToken, String qrStr) {
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(SERVER_URL);
+        String jsonStr = "{\"uuid\":\"" + qrStr + "\",\"access_token\":\"" + accessToken + "\"}";
+        Log.v("##", " JSON to post:" + jsonStr);
+        try {
+            StringEntity se = new StringEntity(jsonStr);
+            httpPost.setEntity(se);
+            HttpResponse response = httpClient.execute(httpPost);
+            Log.v("###", "Respnse:" + response.toString());
+            showText("Successfully posted token");
+
+        } catch (ClientProtocolException e) {
+            showText("Unable to Post Token");
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            showText("Unable to Post Token");
+            e.printStackTrace();
+        } catch (IOException e) {
+            showText("Unable to Post Token");
+            e.printStackTrace();
+        }
+
+    }
+}
+
---
+++ app/src/main/java/com/enterprise/donorapplication/MainActivity.java
@@ -0,0 +1,123 @@
+package com.enterprise.donorapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
+import com.enterprise.donorapplication.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    private TextView greeting;
+    private Button scanButton;
+    private EditText _emailText;
+    private EditText _passwordText;
+    private Button _loginButton;
+    private TextView _signupLink;
+
+    public MainActivity() {
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        _emailText=(EditText)findViewById(R.id.input_email);
+        _passwordText=(EditText)findViewById(R.id.input_password);
+        _loginButton=(Button)findViewById(R.id.btn_login);
+
+
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+
+    }
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        _loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this,
+                R.style.AppTheme);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        return valid;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        // disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        _loginButton.setEnabled(true);
+        finish();
+    }
+
+    public void onLoginFailed() {
+
+        _loginButton.setEnabled(true);
+    }
+
+
+}
